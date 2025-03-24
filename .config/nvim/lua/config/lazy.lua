b63b2feab0a2820b0aa413070d1bd452e0f1286a
amorimll <gabrielmaia.amorim01@gmail.com>
2025-03-24 00:31:48 -0300

    lazyvim setup with nord colorscheme

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "tokyonight" } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "nord" } },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -30,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "tokyonight", "habamax", "nordic", "ashen", "minimal", "kanagawa-paper", "rose-pine", "nord" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,21 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true;
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "VertSplit", { bg = "NONE", fg = "NONE" })
+    vim.api.nvim_set_hl(0, "WinSeparator", { bg = "NONE", fg = "#2E3440" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { fg = "NONE", bg = "#2E3440" })
+  end,
+})
+
+-- Torna os separadores transparentes
+--vim.api.nvim_set_hl(0, "VertSplit", { bg = "NONE", fg = "NONE" })
+
+--vim.opt.fillchars = {
+--  vert = " ",  -- Separador vertical como espaço em branco
+--  horiz = " ", -- Separador horizontal como espaço em branco
+--  eob = " ",   -- Fim do buffer como espaço em branco
+--}
